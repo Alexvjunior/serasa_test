a50ad0f494bf2894c02ee123331130bb89b7ebae
Alexsander Junior <alexsander.junior@luizalabs.com>
2023-05-15 16:33:27 -0300

    Finish tests for user_api app

--- apps/user_api/views.py
+++ apps/user_api/views.py
@@ -4,6 +4,7 @@ from apps.user_api.models import User
 
 
 class UserAPIViewSet(viewsets.ModelViewSet):
+    basename = 'user'
     serializer_class = UserAPISerializer
     queryset = User.objects.all()
     http_method_names = ["post", "patch", "get", "delete"]
