eedcb3986f57322dc01b93b80ba8f6ec83d4c097
Alexsander Junior <alexsander.junior@luizalabs.com>
2023-05-15 14:52:21 -0300

    Star Project in django and create app user_api

---
+++ apps/user_api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+class User(TimeStampedModel):
+
+    name = models.CharField(max_length=255)
+
+    cpf = models.CharField(max_length=11, unique=True)
+
+    email = models.EmailField(unique=True)
+
+    phone_number = models.CharField(max_length=20)
+
+
+
+
+
