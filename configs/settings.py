8f262ed70e7ff1c9778a93e5a2f68d8c9cf04088
Alexsander Junior <alexsander.junior@luizalabs.com>
2023-05-16 15:22:47 -0300

    Config postgress in project

--- configs/settings.py
+++ configs/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'db_password',
+        'HOST': 'db_host',
+        'PORT': 'db_port',
     }
 }
 
